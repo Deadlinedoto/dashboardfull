b71bbd5c63fd937694301bdfe9a8f535090da952
Deadlinedoto <avalanchedoto@mail.ru>
2024-09-10 01:48:00 +0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/header/header.component";
 import {MainComponent} from "./components/main/main.component";
 import {BodyComponent} from "./components/body/body.component";
+import {MyAddsComponent} from "./components/my-adds/my-adds.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, BodyComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, BodyComponent, MyAddsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/my-adds/my-adds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-adds',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-adds.component.html',
+  styleUrl: './my-adds.component.scss'
+})
+export class MyAddsComponent {
+
+}
