0861354389e77d0adb4f2b23f8ca42ea7b775803
Deadlinedoto <avalanchedoto@mail.ru>
2024-09-10 00:26:56 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {MainComponent} from "./components/main/main.component";
+import {BodyComponent} from "./components/body/body.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
